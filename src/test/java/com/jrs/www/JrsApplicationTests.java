0fb8a73b74460a81bc51458f9ce76c5f369fcb3e
xpb_12345@163.com <xpb_12345@163.com>
2018-05-23 18:47:11 +0800

    【作者】薛蓬勃 【描述】 jrs直播框架

---
+++ src/test/java/com/jrs/www/JrsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jrs.www;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JrsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
