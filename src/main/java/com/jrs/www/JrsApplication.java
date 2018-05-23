4fa8a0b36a53f3a6a0f89a98d214701fcf9530bf
xpb_12345@163.com <xpb_12345@163.com>
2018-05-23 14:01:09 +0800

    【作者】薛蓬勃 【描述】 jrs直播框架

---
+++ src/main/java/com/jrs/www/JrsApplication.java
@@ -0,0 +1,12 @@
+package com.jrs.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JrsApplication.class, args);
+	}
+}
